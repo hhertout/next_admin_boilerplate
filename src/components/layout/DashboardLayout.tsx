bd50006cd656c67fb6fcd4c91c10351ebfc017f1
hhertout <hugues.hertout@gmail.com>
2024-01-29 21:51:48 +0100

    feat: introduce layout

---
+++ src/components/layout/DashboardLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// to fix in props
+const asideNav = []
+
+const DashboardLayout = () => {
+  // TODO
+  return (
+    <main>
+      <aside>
+        <nav>
+
+        </nav>
+      </aside>
+      <section>
+
+      </section>
+    </main>
+  );
+};
+
+export default DashboardLayout;
